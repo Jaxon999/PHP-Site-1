d04730cdc3706c42bde9673f8b815109a3d20c83
Jaxon999 <35505280+Jaxon999@users.noreply.github.com>
2022-04-27 16:38:22 -0500

    Delete footer.php

--- footer.php
+++
@@ -1,8 +0,0 @@
-<div class="footer">
-	<center><a href="Login.php">Login</a></center>
-    <center><a href="Welcome.php">Welcome</a></center>
-    <center><a href="Cart.php">Cart</a></center>
-    <center><a href="product-1.php">Products</a></center>
-</div>
-</body>
-</html>
